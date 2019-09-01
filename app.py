5c46ed4721db99946bd2929f304d3bcc10b2ddb7
BurbaCodes <dilushadasanayaka@gmail.com>
2019-09-01 19:06:30 +0530

    changes

--- app.py
+++ app.py
@@ -21,6 +21,9 @@ import datetime
 root = Tk()
 root.geometry("800x400")
 
+path = 'videos/p2.mp4'
+vs = cv2.VideoCapture(path)
+
 def updateDeleteSlot(frame):
     for widget in frame.winfo_children():
         widget.destroy()
@@ -111,12 +114,31 @@ def mousePosition(event,x,y,flags,param):
         print(x,y)
         param = (x,y)
 
-def generateVideo(boxes):
-    path = 'videos/p2.mp4'
-    vs = cv2.VideoCapture(path)
+## check whether the given value is minimun in array
+def checkMinimum(list1, val): 
+      
+    # traverse in the list 
+    for x in list1: 
+  
+        # compare with all the values 
+        # with val 
+        if val>= x: 
+            return False 
+    return True
+
+## update status window
+def updateStatusWindow(frame):
+    for item in frame.winfo_children():
+        item.destroy()
+
+    Label(frame, text="test", height=2).pack()
+    frame.pack()
+
+def generateVideo(boxes, statusWindow):
+    # path = 'videos/p2.mp4'
+    # vs = cv2.VideoCapture(path)
     fps = 12
     capSize = (640,360)
-    #capSize = (1920,1080)
     #fourcc = cv2.VideoWriter_fourcc(*'DIVX')
     fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
     #fourcc = cv2.VideoWriter_fourcc(*'XVID')
@@ -124,7 +146,8 @@ def generateVideo(boxes):
     success = out.open('./teste5.mp4',fourcc,fps,capSize,True)
 
     num_frames = count_frames(path)
-    print(num_frames)
+    #print(num_frames)
+
 
     get_point = 0  
     i = 0 
@@ -151,9 +174,9 @@ def generateVideo(boxes):
             print(boxes)
             check = False
 
-        else:
-            box1 = np.array([[130.14516129032256, 226.74999999999994], [85.68951612903224, 223.12096774193543], [121.97983870967742, 175.0362903225806], [155.5483870967742, 179.57258064516125]])       
-            box2 = np.array ([[137.4032258064516, 225.84274193548384], [166.43548387096774, 173.22177419354836], [210.89112903225805, 177.758064516129], [196.375, 226.74999999999994]])
+        #else:
+            # box1 = np.array([[130.14516129032256, 226.74999999999994], [85.68951612903224, 223.12096774193543], [121.97983870967742, 175.0362903225806], [155.5483870967742, 179.57258064516125]])       
+            # box2 = np.array ([[137.4032258064516, 225.84274193548384], [166.43548387096774, 173.22177419354836], [210.89112903225805, 177.758064516129], [196.375, 226.74999999999994]])
             
             #boxes = [box1,box2]
             #print(boxes)
@@ -172,21 +195,41 @@ def generateVideo(boxes):
 
         score = SVM().predict(feature)
 
+        available_slots = []
+
         for index,scr in enumerate(score):
             if scr == 0: 
                 cv2.polylines(frame,np.int32([boxes[index]]), True ,(0,0,255),2  )
                 saida = False
                 i = 0
-
             else:
-                cv2.polylines(frame,np.int32([boxes[index]]),True,(0,255,0), 2)
+                ## if the parking slot is availble
 
-                if saida == False:
-                    cv2.putText(frame, timestamp.strftime(" Saida as: %d %m %Y %I:%M:%S"), (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.65, (0, 255, 0),2)
-                    i += 1
-                    if i > 100: 
-                        saida = True
+                slot_first_x_cord = float(boxes[index][1][0])
 
+                if(checkMinimum(available_slots, slot_first_x_cord)):
+                    cv2.polylines(frame,np.int32([boxes[index]]),True,(255,0,0), 2)
+
+                    if saida == False:
+                        cv2.putText(frame, timestamp.strftime(" Saida as: %d %m %Y %I:%M:%S"), (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.65, (255,0,0),2)
+                        i += 1
+                        if i > 100: 
+                            saida = True
+                
+                else:
+                    cv2.polylines(frame,np.int32([boxes[index]]),True,(0,255,0), 2)
+
+                    if saida == False:
+                        cv2.putText(frame, timestamp.strftime(" Saida as: %d %m %Y %I:%M:%S"), (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.65, (0,255,0),2)
+                        i += 1
+                        if i > 100: 
+                            saida = True                    
+
+
+                available_slots.append(slot_first_x_cord)
+
+
+        print(available_slots)
 
         # if score[1] == 0: 
         #     cv2.polylines(frame,np.int32([box2]), True ,(0,0,255),2  )
@@ -220,6 +263,8 @@ def generateVideo(boxes):
 left = Frame(root, borderwidth=2, relief="solid")
 right = Frame(root, borderwidth=2, relief="solid")
 container = Frame(left, borderwidth=2, relief="solid")
+
+statusWindow = Frame(right, borderwidth=2, relief="solid")
 box1 = Listbox(right, borderwidth=2, relief="solid")
 box2 = Frame(right, borderwidth=2, relief="solid")
 
@@ -262,7 +307,7 @@ lable6 = Label(box2, text="Add new parking slot").grid(row=1, column=0, columnsp
 btnAddCord = Button(box2, text= "Add Slot", cursor= "hand2", command= lambda: btn_add_slot()).grid(row=1, column=3, padx=5, pady=5)
 
 lable6 = Label(box2, text="Open Monitor").grid(row=2, column=0, columnspan=3, padx=5, pady=5)
-btnAddCord = Button(box2, text= "Open", cursor= "hand2", command= lambda: generateVideo(boxes)).grid(row=2, column=3, padx=5, pady=5)
+btnAddCord = Button(box2, text= "Open", cursor= "hand2", command= lambda: generateVideo(boxes, statusWindow)).grid(row=2, column=3, padx=5, pady=5)
 
 
 left.pack(side="left", expand=True, fill="both")
