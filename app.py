0acfc5481e3346c18c5bbee15042c44c964bb054
BurbaCodes <dilushadasanayaka@gmail.com>
2019-09-03 06:08:11 +0530

    align code

--- app.py
+++ app.py
@@ -149,9 +149,9 @@ def generateVideo(boxes, statusWindow):
     vs = cv2.VideoCapture(path)
     fps = 12
     capSize = (640,360)
-    #fourcc = cv2.VideoWriter_fourcc(*'DIVX')
+
     fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
-    #fourcc = cv2.VideoWriter_fourcc(*'XVID')
+
     out = cv2.VideoWriter()
     success = out.open('./teste5.mp4',fourcc,fps,capSize,True)
 
@@ -161,117 +161,82 @@ def generateVideo(boxes, statusWindow):
     get_point = 0  
     i = 0 
 
-    while i < num_frames:
-        ret, frame = vs.read()
+    try:
+        while i < num_frames:
+            ret, frame = vs.read()
 
-        frame = imutils.resize(frame, width=450)  
+            frame = imutils.resize(frame, width=450)  
 
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        #cv2.imshow("gray ", gray )
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            #cv2.imshow("gray ", gray )
 
-        gau = cv2.GaussianBlur(gray, (7, 7), 0)
-        #cv2.imshow("gau ", gau )
+            gau = cv2.GaussianBlur(gray, (7, 7), 0)
 
-        # Not work yet
+            img_resize = image_utils.getRotateRect(gau, boxes)
+            feature = image_utils().extract_features(img_resize)
 
-        if get_point == True:
-            number_of_places = 2 # Defini o numero de vagas que voce ira selecionar 
-            boxes = image_utils.getSpotsCoordiantesFromImage(frame, number_of_places)
-            boxes = asarray(boxes)
-            print(boxes)
-            check = False
+            timestamp = datetime.datetime.now()
 
-        #else:
-            # box1 = np.array([[130.14516129032256, 226.74999999999994], [85.68951612903224, 223.12096774193543], [121.97983870967742, 175.0362903225806], [155.5483870967742, 179.57258064516125]])       
-            # box2 = np.array ([[137.4032258064516, 225.84274193548384], [166.43548387096774, 173.22177419354836], [210.89112903225805, 177.758064516129], [196.375, 226.74999999999994]])
-            
-            #boxes = [box1,box2]
-            #print(boxes)
-        #print('afdasdf')
-        #print(box1[0].shape)
+            score = SVM().predict(feature)
 
-        img_resize = image_utils.getRotateRect(gau, boxes)
-        feature = image_utils().extract_features(img_resize)
+            available_slots = []
 
-        '''feature1 = feature.reshape(-1, 1)
-        score0 = SVM().predict(feature1)
-        score1 = SVM().predict(feature[1])
-
-        print (score0, score1)'''
-        timestamp = datetime.datetime.now()
-
-        score = SVM().predict(feature)
-
-        available_slots = []
-
-        for index,scr in enumerate(score):
-            if scr == 0: 
-                cv2.polylines(frame,np.int32([boxes[index]]), True ,(0,0,255),2  )
-                saida = False
-                i = 0
-            else:
-                ## if the parking slot is availble
-
-                slot_first_x_cord = float(boxes[index][1][0])
-
-                if(checkMinimum(available_slots, slot_first_x_cord)):
-                    cv2.polylines(frame,np.int32([boxes[index]]),True,(255,0,0), 2)
-
-                    if saida == False:
-                        cv2.putText(frame, timestamp.strftime(" Saida as: %d %m %Y %I:%M:%S"), (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.65, (255,0,0),2)
-
-                        i += 1
-                        if i > 100: 
-                            saida = True
-                
+            for index,scr in enumerate(score):
+                if scr == 0: 
+                    cv2.polylines(frame,np.int32([boxes[index]]), True ,(0,0,255),2  )
+                    s = False
+                    i = 0
                 else:
-                    cv2.polylines(frame,np.int32([boxes[index]]),True,(0,255,0), 2)
+                    ## if the parking slot is availble
 
-                    if saida == False:
-                        cv2.putText(frame, timestamp.strftime(" Available: %d %m %Y %I:%M:%S"), (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.65, (0,255,0),2)
-                        i += 1
-                        if i > 100: 
-                            saida = True 
+                    slot_first_x_cord = float(boxes[index][1][0])
 
-                msg = "Nearest Parking slot is " + str(index)
-                clr = 'green'
-                updateStatusWindow(msg,clr)                   
+                    if(checkMinimum(available_slots, slot_first_x_cord)):
+                        cv2.polylines(frame,np.int32([boxes[index]]),True,(255,0,0), 2)
 
-                available_slots.append(slot_first_x_cord)
+                        if s == False:
+                            cv2.putText(frame, timestamp.strftime(" Available: %d %m %Y %I:%M:%S"), (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.65, (255,0,0),2)
 
-        if not available_slots:
-            msg = "Parking slots are not available " 
-            clr = 'red'
-            updateStatusWindow(msg,clr)
+                            i += 1
+                            if i > 100: 
+                                s = True
+                    
+                    else:
+                        cv2.polylines(frame,np.int32([boxes[index]]),True,(0,255,0), 2)
+
+                        if s == False:
+                            cv2.putText(frame, timestamp.strftime(" Available: %d %m %Y %I:%M:%S"), (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.65, (0,255,0),2)
+                            i += 1
+                            if i > 100: 
+                                s = True 
+
+                    msg = "Parking slots are available "
+                    clr = 'green'
+                    updateStatusWindow(msg,clr)                   
+
+                    available_slots.append(slot_first_x_cord)
+
+            if not available_slots:
+                msg = "Parking slots are not available " 
+                clr = 'red'
+                updateStatusWindow(msg,clr)     
+
+            cv2.imshow("frame", frame)
+
+            key = cv2.waitKey(10) & 0xFF
+
+            if key == ord("q"):
+                break
+
+            if key == ord('p'):
+                hist = cv2.calcHist([a[1]], [0], None, [256], [0,256])
+                plt.plot(hist)
+                plt.show()
 
 
-        # if score[1] == 0: 
-        #     cv2.polylines(frame,np.int32([box2]), True ,(0,0,255),2  )
-            
-
-
-        # else:
-        #     cv2.polylines(frame,np.int32([box2]),True,(0,255,0), 2)
-        #     cv2.putText(frame, timestamp.strftime(" %d %B %Y %I:%M:%S%p"), (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 2.35, (0, 0, 255), 5)
-
-
-
-        
-
-        cv2.imshow("frame", frame)
-
-        key = cv2.waitKey(1) & 0xFF
-        # Se a tecla 'q' for pressionada, finaliza com o loop
-        if key == ord("q"):
-            break
-
-        if key == ord('p'):
-            hist = cv2.calcHist([a[1]], [0], None, [256], [0,256])
-            plt.plot(hist)
-            plt.show()
-
-
-        i += 1 
+            i += 1 
+    except KeyboardInterrupt:
+        pass
 
 def loadVideo(statusWindow):
     c = getCordData()
@@ -320,8 +285,8 @@ label5 = Label(box2, text="Controls").grid(row=0, column=0)
 
 lable6 = Label(box2, text="Add new parking slot").grid(row=1, column=0, columnspan=3, padx=5, pady=5)
 txtSlotName = Entry(box2)
-txtSlotName.grid(row=1, column=3, columnspan=3, padx=5, pady=5)
-btnAddCord = Button(box2, text= "Add Slot", cursor= "hand2", command= lambda: btn_add_slot(txtSlotName.get())).grid(row=1, column=6, padx=5, pady=5)
+txtSlotName.grid(row=1, column=3, columnspan=1, padx=5, pady=5)
+btnAddCord = Button(box2, text= "Add Slot", cursor= "hand2", command= lambda: btn_add_slot(txtSlotName.get())).grid(row=1, column=4, padx=5, pady=5)
 
 # lable6 = Label(box2, text="Reset new parking slot Coordinates").grid(row=2, column=0, columnspan=3, padx=5, pady=5)
 # btnAddCord = Button(box2, text= "Reset", cursor= "hand2", command= lambda: btn_reset_slot()).grid(row=2, column=3, padx=5, pady=5)
